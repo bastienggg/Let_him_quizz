fa0d0701d8f06cf35b84d437d70e68e31979dba2
EnguerranCA <enguerran.caroalquier@etu.unilim.fr>
2025-02-07 11:20:49 +0100

    Money update and fps counter

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import { MCQ } from './data/data-mcq.js';
 
 // Components imports
 import { TickingAway } from './components/ticking-away/ticking-away.js';
+import { Money } from './components/money-counter/money-counter.js';
 import { DebugMenu } from './components/debug-menu/debug-menu.js';
 
 TickingAway.renderQuizZone();
 TickingAway.newQuestion();
+
+Money.renderMoneyZone();
+
+
+Money.summonStack(100);

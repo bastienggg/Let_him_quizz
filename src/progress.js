066bd4f8f59b146bf989d71e62ff0f4324c85b7d
bastienggg <bastien.guitard@etu.unilim.fr>
2025-02-11 12:39:28 +0100

    Refactor progress bar implementation to use setInterval for smoother updates; enhance scene loading logic

--- src/progress.js
+++ src/progress.js
@@ -1,24 +1,42 @@
+
 import { Camera } from './components/camera/camera.js';
 
 document.addEventListener("DOMContentLoaded", function () {
+
     const progressBar = document.getElementById("progress-bar");
-    const assets = document.querySelector("a-assets");
 
-    assets.addEventListener("progress", function (event) {
-        const progress = event.detail.loaded / event.detail.total;
-        progressBar.style.width = (progress * 100) + "%";
-    });
+    let width = 0;
 
-    assets.addEventListener("loaded", function () {
-        document.getElementById("progress-bar-container").style.display = "none";
-        const scene = document.querySelector("a-scene");
-        scene.style.display = "block";
-        scene.setAttribute("cursor", "rayOrigin: mouse;");
+    const interval = setInterval(function () {
 
-        setTimeout(function () {
-            console.log('Déplacer le joueur en 8 secondes');
-            console.log('fix');
-            Camera.moveCameraVR({ x: 0, y: 2.2, z: 0 }, { x: 1.237, y: 3, z: -35 }, 8000);
-        }, 500);
-    });
+        if (width >= 100) {
+
+            clearInterval(interval);
+
+            document.getElementById("progress-bar-container").style.display = "none";
+
+            const scene = document.querySelector("a-scene");
+            scene.style.display = "block";
+            scene.setAttribute("cursor", "rayOrigin: mouse; ");
+
+
+
+            setTimeout(function () {
+                console.log('Déplacer le joueur en 8 secondes');
+                Camera.moveCameraVR({ x: 0, y: 2.2, z: 0 }, { x: 1.237, y: 3, z: -35 }, 8000);
+                // Camera.moveCamera(8000, [0, 2.2, 0], [1.237, 3, -35.03326]);
+            }, 500);
+
+
+        } else {
+
+            width++;
+
+            progressBar.style.width = width + "%";
+
+        }
+
+    }, 10); // 10ms interval for smoother progress
+
+
 });

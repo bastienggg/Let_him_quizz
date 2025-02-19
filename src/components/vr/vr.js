cf8a60128ae8da1e5173d3d29c654ac3ce88ba42
bastienggg <bastien.guitard@etu.unilim.fr>
2025-02-19 13:06:12 +0100

    Add draggable functionality to VR objects and change box color to blue

--- src/components/vr/vr.js
+++ src/components/vr/vr.js
@@ -1,3 +1,40 @@
+AFRAME.registerComponent('draggable', {
+    init: function () {
+        const el = this.el;
+        let isDragging = false;
+        let grabbedObject = null;
+        let offset = new THREE.Vector3();
+
+        el.addEventListener('selectstart', function () {
+            let intersectedEl = el.components.raycaster.intersectedEls[0];
+
+            if (intersectedEl) {
+                grabbedObject = intersectedEl;
+                grabbedObject.setAttribute('dynamic-body', 'mass: 0');
+                let objPos = grabbedObject.object3D.position.clone();
+                let controllerPos = el.object3D.position.clone();
+                offset.copy(objPos).sub(controllerPos);
+                isDragging = true;
+            }
+        });
+
+        el.addEventListener('axismove', function () {
+            if (isDragging && grabbedObject) {
+                let controllerPos = el.object3D.position;
+                grabbedObject.object3D.position.copy(controllerPos).add(offset);
+            }
+        });
+
+        el.addEventListener('selectend', function () {
+            if (grabbedObject) {
+                grabbedObject.setAttribute('dynamic-body', 'mass: 5');
+                grabbedObject = null;
+                isDragging = false;
+            }
+        });
+    }
+});
+
 let Vr = {};
 
 Vr.setupControllerClickHandler = function (controllerSelector) {
